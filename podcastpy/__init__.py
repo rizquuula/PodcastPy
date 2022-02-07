e0d49ff35f9de8606fb02d4880c4ab023b6f4aa8
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2022-02-07 18:25:54 +0700

    add docstring

--- podcastpy/__init__.py
+++ podcastpy/__init__.py
@@ -8,10 +8,18 @@ Created on Fri 02 15:03:00 2022
 from .podcastpy import PodcastPy
 
 
-def auto_trimmer(original_video_path, result_video_path, time_margin_in_second=0.25, hist_sampling_data=50):
+def auto_trimmer(original_video_path:str, result_video_path:str, time_margin_in_second=0.25, hist_sampling_data=50):
+    """
+    Auto trim video to remove audio noise and blank using PodcastPy
+
+    #### Args:
+        original_video_path (str): Original video to be processed
+        result_video_path (str): Video result path
+        time_margin_in_second (float, optional): Minimum time between the sound gap. Defaults to 0.25 seconds.
+        hist_sampling_data (int, optional): Histogram sampling data, used on noise removal process. Defaults to 100.
+    """
     app = PodcastPy()
-    
-    app.main(original_video_path, 
+    app.auto_trimmer(original_video_path, 
              result_video_path, 
              time_margin_in_second, 
              hist_sampling_data)
