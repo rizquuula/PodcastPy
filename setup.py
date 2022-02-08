02e094e0668df1d8cdcae46f289efe4e6cab6063
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2022-02-08 15:01:30 +0700

    update requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from podcastpy import __version__
 import pathlib
 
 BASE_DIR = pathlib.Path(__file__).parent
@@ -6,7 +7,7 @@ README = (BASE_DIR / "README.md").read_text()
 
 setup(
     name="podcastpy",
-    version="0.0.1",
+    version=__version__,
     author="M Razif Rizqullah",
     author_email="razifrizqullah@gmail.com",
     url='https://github.com/eiproject/PodcastPy',
@@ -17,30 +18,11 @@ setup(
     packages=['podcastpy'],
     include_package_data=True,
     install_requires=[
-            'certifi>=2021.10.8',
-            'charset-normalizer>=2.0.11',
-            'colorama>=0.4.4',
-            'cycler>=0.11.0',
-            'decorator>=4.4.2',
-            'fonttools>=4.29.1',
-            'idna>=3.3',
-            'imageio>=2.14.1',
-            'imageio-ffmpeg>=0.4.5',
-            'kiwisolver>=1.3.2',
-            'matplotlib>=3.5.1',
-            'moviepy>=1.0.3',
-            'numpy>=1.22.2',
-            'packaging>=21.3',
-            'Pillow>=9.0.1',
-            'proglog>=0.1.9',
-            'pydub>=0.25.1',
-            'pyparsing>=3.0.7',
-            'python-dateutil>=2.8.2',
-            'requests>=2.27.1',
-            'scipy>=1.7.3',
-            'six>=1.16.0',
-            'tqdm>=4.62.3',
-            'urllib3>=1.26.8'
+                'tqdm',
+                'numpy',
+                'scipy',
+                'matplotlib',
+                'pydub',
             ],
     keywords=['python', 'glrlm', 'feature extraction', 'image processing'],
     classifiers=[
